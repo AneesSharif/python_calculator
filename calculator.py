3fdd22f42f25b06163fb58a246f46ccef70be5cc
Anees Sharif <manees.work@gmail.com>
2024-07-17 06:52:19 +0500

    raw calculator file

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# This just working test for git learning materia
+
+def add(x,y):
+    """"add function"""
+    pass
+def subtract(x,y):
+    """"subtract function"""
+    pass
+def multiply(x,y):
+    """"multiply function"""
+    pass
+def divide(x,y):
+    """"divide function"""
+    pass
+def squre_root(x,y):
+    """"square root function"""
+    pass
