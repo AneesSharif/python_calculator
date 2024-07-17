b48386c667b3743f06e130bb6c485d1c19a36783
Anees Sharif <manees.work@gmail.com>
2024-07-17 06:57:45 +0500

    add spaces among functions

--- calculator.py
+++ calculator.py
@@ -3,15 +3,19 @@
 def add(x,y):
     """"add function"""
     pass
+
 def subtract(x,y):
     """"subtract function"""
     pass
+
 def multiply(x,y):
     """"multiply function"""
     pass
+
 def divide(x,y):
     """"divide function"""
     pass
+
 def squre_root(x,y):
     """"square root function"""
     pass
